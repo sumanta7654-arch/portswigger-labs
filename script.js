f2e20d6459bacec2d985e4bc4af8f0a0201ebd3c
aroy29822-crypto <aroy29822@gmail.com>
2026-04-12 22:52:41 +0530

    initial cyberwriteup labs

---
+++ script.js
@@ -0,0 +1,54 @@
+// Typing
+const text = "Sumanta";
+let i = 0;
+
+function typeEffect() {
+    if (i < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeEffect, 100);
+    }
+}
+typeEffect();
+
+// Scroll animation
+function revealOnScroll() {
+    const reveals = document.querySelectorAll(".reveal");
+
+    reveals.forEach((el) => {
+        const windowHeight = window.innerHeight;
+        const elementTop = el.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - 100) {
+            el.classList.add("active");
+        }
+    });
+}
+window.addEventListener("scroll", revealOnScroll);
+
+// Terminal
+const terminalText = "Accessing system...\nWelcome, Red Teamer 😈";
+let j = 0;
+
+function terminalEffect() {
+    if (j < terminalText.length) {
+        document.getElementById("terminal-text").innerHTML += terminalText.charAt(j);
+        j++;
+        setTimeout(terminalEffect, 30);
+    }
+}
+terminalEffect();
+function revealOnScroll() {
+    const reveals = document.querySelectorAll(".reveal");
+
+    reveals.forEach((el) => {
+        const windowHeight = window.innerHeight;
+        const elementTop = el.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - 80) {
+            el.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
