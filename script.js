1e11403ff84654836fa132b064ebe8b71f27d71e
aroy29822-crypto <aroy29822@gmail.com>
2026-04-24 15:45:07 +0530

    more writeups and more fixes

--- script.js
+++ script.js
@@ -11,19 +11,6 @@ function typeEffect() {
 }
 typeEffect();
 
-// Scroll animation
-function revealOnScroll() {
-    const reveals = document.querySelectorAll(".reveal");
-
-    reveals.forEach((el) => {
-        const windowHeight = window.innerHeight;
-        const elementTop = el.getBoundingClientRect().top;
-
-        if (elementTop < windowHeight - 100) {
-            el.classList.add("active");
-        }
-    });
-}
 window.addEventListener("scroll", revealOnScroll);
 
 // Terminal
